56bfb6aff9c38dc716493ec7da6086c2307d97a3
Josué Estrada <dev.josuestrd@gmail.com>
2024-05-15 22:40:41 -0500

    Agregado sidebar con switch theme

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,8 +14,9 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
-import ModeToggle from "./theme-toggle-button";
+import { ModeToggle, ModeToggleSidebar } from "./theme-toggle-button";
 
 export default function Navbar() {
   return (
@@ -84,9 +85,66 @@ export default function Navbar() {
             </DropdownMenu>
             <ModeToggle />
           </nav>
-          <Button className="md:hidden" variant="outline">
-            <MenuIcon className="h-6 w-6" />
-          </Button>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
+              >
+                <MenuIcon className="h-5 w-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left">
+              <nav className="grid gap-6 text-lg font-medium">
+                <Link
+                  href="/"
+                  className="flex items-center gap-2 text-lg font-semibold"
+                >
+                  <MountainIcon className="h-6 w-6" />
+                  <span className="sr-only">Solinox</span>
+                </Link>
+                <Link
+                  href="/"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  Inicio
+                </Link>
+                <Link
+                  href="/nosotros"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  Nosotros
+                </Link>
+                <Link
+                  href="/certificaciones"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  Certificaciones
+                </Link>
+                <Link
+                  href="/servicios"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  Servicios
+                </Link>
+                <Link
+                  href="/blog"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  Blog
+                </Link>
+                <Link
+                  href="/contacto"
+                  className="text-muted-foreground hover:text-foreground"
+                >
+                  Contacto
+                </Link>
+                <ModeToggleSidebar />
+              </nav>
+            </SheetContent>
+          </Sheet>
         </div>
       </header>
     </>
--- src/components/theme-toggle-button.tsx
+++ src/components/theme-toggle-button.tsx
@@ -12,7 +12,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-export default function ModeToggle() {
+export function ModeToggle() {
   const { setTheme } = useTheme();
 
   return (
@@ -22,6 +22,49 @@ export default function ModeToggle() {
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Claro
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Oscuro
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          Sistema
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export function ModeToggleSidebar() {
+  const { theme, setTheme } = useTheme();
+
+  const getButtonText = () => {
+    switch (theme) {
+      case "light":
+        return "Tema Claro";
+      case "dark":
+        return "Tema Oscuro";
+      case "system":
+        return "Sistema";
+      default:
+        return "Tema";
+    }
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className=" justify-normal text-lg font-medium text-muted-foreground hover:text-foreground"
+        >
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="ml-2">{getButtonText()}</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
